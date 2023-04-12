de7d1da6ffbc03e672b0e99256a74308bfea8c88
ilovenms <ilovenmslmao@proton.me>
2023-04-12 09:49:40 +0300

    new changes

--- lab1.py
+++ lab1.py
@@ -1,8 +1,14 @@
-# Calc Cube using function
+# Function Menu Driven Airthmetic Operation
 # Credits: Dhruv , XII-D
 
-def cube(n):
-    c = n*n*n
-    return c
-n1 = int(input('Enter Num: '))
-print(cube(n1))
+def get_result(type_of_operation,num1,num2):
+    if type_of_operation == "+":return num1+num2
+    elif type_of_operation == "-":return num1-num2
+    elif type_of_operation == "*":return num1*num2
+    elif type_of_operation == "/":return num1/num2
+    
+ops = str(input("Ops: "))
+num1 = int(input("Num1 : "))
+num2 = int(input("Num2 : "))
+
+print(get_result(ops,num1,num2))
